9b753c12faccf89d00d5b9877acfbdbdf8d5acf7
huangjs888 <1170384651@qq.com>
2023-11-29 15:01:01 +0800

    update

--- src/datetimepicker/useDerivedState.ts
+++ src/datetimepicker/useDerivedState.ts
@@ -2,23 +2,49 @@
  * @Author: Huangjs
  * @Date: 2022-11-23 14:22:25
  * @LastEditors: Huangjs
- * @LastEditTime: 2023-01-17 16:25:44
+ * @LastEditTime: 2023-09-13 16:00:53
  * @Description: ******
  */
 import React from 'react';
 
-type Param<T> = { value: T };
-export function useDerivedState<D>(vv: D): [D, (v: D) => void] {
-  const preValueRef = React.useRef<D>(vv);
-  const [value, setValue] = React.useState<Param<D>>({ value: vv });
-  let mergedValue: D = value.value;
-  if (vv !== preValueRef.current) {
-    preValueRef.current = vv;
-    mergedValue = vv;
+export function useChanged<T>(
+  value: T,
+  compare = (prev: T | void, next: T) => prev !== next,
+): boolean {
+  const propsRef = React.useRef<T>(value);
+  const changed = compare(propsRef.current, value);
+  if (changed) {
+    propsRef.current = value;
   }
-  const changeState = React.useCallback<(v: D) => void>(
-    (v: D) => setValue({ value: v }),
-    [],
+  return changed;
+}
+
+export function useForceUpdate() {
+  const setState = React.useState({})[1];
+  return React.useCallback(() => setState({}), [setState]);
+}
+
+type Updater<T> = (updater: T | ((origin: T) => T)) => void;
+export function useDerivedState<T>(
+  value: T,
+  useCompare = (prev: T, next: T) => (useChanged(next) ? next : prev),
+): [T, Updater<T>] {
+  const nowValueRef = React.useRef<T>(value);
+  nowValueRef.current = useCompare(nowValueRef.current, value);
+  const forceUpdate = useForceUpdate();
+  const updateValue: Updater<T> = React.useCallback(
+    (v) => {
+      const _value =
+        typeof v === 'function'
+          ? (v as (origin: T) => T)(nowValueRef.current)
+          : v;
+      // 是否需要比较？
+      if (_value !== nowValueRef.current) {
+        nowValueRef.current = _value;
+        forceUpdate();
+      }
+    },
+    [forceUpdate],
   );
-  return [mergedValue, changeState];
+  return [nowValueRef.current, updateValue];
 }
